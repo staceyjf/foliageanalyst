2f49072e31bdd6f7b75e9e143deb24f58364aa9d
staceyjf <staceyfanner@gmail.com>
2023-08-11 16:47:52 +1000

    About & Index page adds
    URLs, Views and template
    
    [Ticket: 2]

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+# setting up the path and connecting to the views
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('plants/', views.plants_index, name='index'),
+]
