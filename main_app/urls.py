bcadc6fa7daa085473dff1e0d609692844a1da62
staceyjf <staceyfanner@gmail.com>
2023-12-19 11:54:35 +1100

    add_photo() add

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     path('plants/<int:pk>/update/', views.PlantUpdate.as_view(), name='plants_update'),
     path('plants/<int:pk>/delete/', views.PlantDelete.as_view(), name='plants_delete'),
     # related tables - PlantCare
-    path('<int:plant_id>/add_care/', views.add_care, name='add_care'),
+    path('plants/<int:plant_id>/add_care/', views.add_care, name='add_care'),
     # associating carers with plants
     path('plants/<int:plant_id>/assoc_carer/<int:carer_id>/', views.assoc_carer, name='assoc_carer'),
     # removing carers with plants
@@ -23,4 +23,6 @@ urlpatterns = [
     path('carers/create/', views.CarerCreate.as_view(), name='carers_create'),
     path('carers/<int:pk>/update/', views.CarerUpdate.as_view(), name='carers_update'),
     path('carers/<int:pk>/delete/', views.CarerDelete.as_view(), name='carers_delete'),
+    # associating carers with photos
+    path('carers/<int:carer_id>/add_photo/', views.add_photo, name='add_photo'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,13 @@
+import os
+import uuid
+import boto3
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 # from bootstrap_datepicker_plus.widgets import DateTimePickerInput
 
 # model import - best practice is to call it singular which I will have to do next time
-from .models import Plants, Carer
+from .models import Plants, Carer, Photo
 from .forms import CareForm
 
 # Created views
@@ -98,3 +101,21 @@ def remove_carer(request, plant_id, carer_id):
   return redirect('details', plant_id=plant_id)
 
   ####### PHOTO  ###########
+def add_photo(request, carer_id):
+  # photo-file will be the "name" attribute on the <input type="file">
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    # creating an unique key for each image while keeping file extension
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      # build the full url string
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      # we can assign to a carer
+      Photo.objects.create(url=url, carer_id=carer_id)
+    except Exception as e:
+      print('An error occurred while uploading the file to S3')
+      print(e)
+  return redirect('carers_detail', carer_id=carer_id)
